4ecd4ee3de6bdfdc1f122b21f05829154f6d4aff
kyle beavin <kylebeavin@gmail.com>
2018-07-16 00:14:49 -0400

    params

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends Component {
@@ -7,6 +7,10 @@ class HomeScreen extends Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button title="Go to Details" onPress={() => this.props.navigation.navigate('Details', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        })} />
       </View>
     );
   }
@@ -14,9 +18,19 @@ class HomeScreen extends Component {
 
 class DetailsScreen extends Component {
   render() {
+
+const { navigation } = this.props;
+const itemId = navigation.getParam('itemId', 'NO-ID');
+const otherParam = navigation.getParam('otherParam', 'some default value');
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button title="Go to Details... again" onPress={() => this.props.navigation.push('Details')} />
+        <Button title="Go to Home" onPress={() => this.props.navigation.navigate('Home')}/>
+        <Button title="Go back" onPress={() => this.props.navigation.goBack()}/>
       </View>
     );
   }
