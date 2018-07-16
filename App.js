01abd8f6016fa804ee0b53ec9a619fb486bda7f8
kyle beavin <kylebeavin@gmail.com>
2018-07-15 23:30:51 -0400

    createStackNavigator

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { createStackNavigator, } from 'react-navigation';
 import { Text, View } from 'react-native';
 import Bananas from './components/Bananas';
 import FixedDimensions from './components/heightandwidth/FixedDimensions';
 import FlexDimensions from './components/heightandwidth/FlexDimensions';
 
-export default class HelloWorldApp extends Component {
+class HomeScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Bananas />
-        <Text>Hello world!</Text>
-        <FixedDimensions />
-        <FlexDimensions />
+      <View style={{ flex:1,alignItems: 'center',justifyContent:'center'}}>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
+
+export default createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
